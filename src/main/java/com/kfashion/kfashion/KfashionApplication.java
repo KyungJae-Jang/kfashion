3189e7fb4dc195b666b8b0e1bbae9229880044d0
KyungJae Jang <chong1175@naver.com>
2020-11-18 13:28:39 +0900

    Initial commit

---
+++ src/main/java/com/kfashion/kfashion/KfashionApplication.java
@@ -0,0 +1,13 @@
+package com.kfashion.kfashion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KfashionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KfashionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kfashion/kfashion/KfashionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kfashion.kfashion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KfashionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
